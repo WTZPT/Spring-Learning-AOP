99071bd81b75c16e3955a40b10af8642191cbf48
Hardy <1277146050@qq.com>
2021-01-02 18:04:39 +0800

    [AOP应用]:对get请求进行拦截输出日志

---
+++ src/main/java/com/example/demo/controller/AopController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AopController {
+    @GetMapping(value = "/getTest")
+    public JSONObject aopTest() {
+        return JSON.parseObject("{\"message\":\"SUCCESS\",\"code\":200}");
+    }
+
+    @PostMapping(value = "/postTest")
+    public JSONObject aopTest2(@RequestParam("id") String id) {
+        return JSON.parseObject("{\"message\":\"SUCCESS\",\"code\":200}");
+    }
+}
