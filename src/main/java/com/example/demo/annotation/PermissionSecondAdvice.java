ce3c641a6b3181a06d283284a7e216e0a235b78f
Hardy <1277146050@qq.com>
2021-01-03 19:30:34 +0800

    自定义注解

---
+++ src/main/java/com/example/demo/annotation/PermissionSecondAdvice.java
@@ -0,0 +1,37 @@
+package com.example.demo.annotation;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(0)
+public class PermissionSecondAdvice {
+    @Pointcut("@annotation(com.example.demo.annotation.PermissionAnnotation)")
+    private void permissionCheck() {
+    }
+
+    @Around("permissionCheck()")
+    public Object permissionCheckSecond(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.println("===================第二个切面===================：" + System.currentTimeMillis());
+
+        //获取请求参数，详见接口类
+        Object[] objects = joinPoint.getArgs();
+        Long id = ((JSONObject) objects[0]).getLong("id");
+        String name = ((JSONObject) objects[0]).getString("name");
+        System.out.println("id->>>>>>>>>>>>>>>>>>>>>>" + id);
+        System.out.println("name->>>>>>>>>>>>>>>>>>>>>>" + name);
+
+        // name不是管理员则抛出异常
+        if (!name.equals("admin")) {
+            return JSON.parseObject("{\"message\":\"not admin\",\"code\":403}");
+        }
+        return joinPoint.proceed();
+    }
+}
--- src/main/java/com/example/demo/controller/AopController.java
+++ src/main/java/com/example/demo/controller/AopController.java
@@ -2,10 +2,8 @@ package com.example.demo.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.annotation.PermissionAnnotation;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AopController {
@@ -16,6 +14,13 @@ public class AopController {
 
     @PostMapping(value = "/postTest")
     public JSONObject aopTest2(@RequestParam("id") String id) {
+        return JSON.parseObject("{\"message\":\"SUCCESS\",\"code\":200}");
+    }
+
+    @PostMapping(value = "/check")
+    // 添加这个注解
+    @PermissionAnnotation()
+    public JSONObject getGroupList(@RequestBody JSONObject request) {
         return JSON.parseObject("{\"message\":\"SUCCESS\",\"code\":200}");
     }
 }
