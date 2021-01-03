52afa9f9d44c0219f89a0fade2bea49261bd72ee
Hardy <1277146050@qq.com>
2021-01-03 20:32:46 +0800

    使用@After和@Before指定的方法在执行之前或之后打印Log

---
+++ src/main/java/com/example/demo/annotation/LogAspectHandler.java
@@ -0,0 +1,33 @@
+package com.example.demo.annotation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class LogAspectHandler {
+    /**
+     * 定义一个切面，拦截 com.mutest.controller 包下的所有方法
+     */
+    @Pointcut("execution(* com.example.demo.controller..*.*(..))")
+    public void pointCut() {}
+
+    /**
+     * 在上面定义的切面方法之后执行该方法
+     * @param joinPoint jointPoint
+     */
+    @After("pointCut()")
+    public void doAfter(JoinPoint joinPoint) {
+
+        log.info("==== doAfter 方法进入了====");
+        Signature signature = joinPoint.getSignature();
+        String method = signature.getName();
+        log.info("方法{}已经执行完", method);
+    }
+}
