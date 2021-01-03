a9355b772982fa820c8782b5eea4a17bc5fbb2de
Hardy <1277146050@qq.com>
2021-01-03 20:39:59 +0800

    使用@AfterReturning对返回结果进行业务增强

--- src/main/java/com/example/demo/annotation/LogAspectHandler.java
+++ src/main/java/com/example/demo/annotation/LogAspectHandler.java
@@ -1,9 +1,11 @@
 package com.example.demo.annotation;
 
+import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
@@ -29,5 +31,16 @@ public class LogAspectHandler {
         Signature signature = joinPoint.getSignature();
         String method = signature.getName();
         log.info("方法{}已经执行完", method);
+    }
+
+    @AfterReturning(pointcut = "pointCut()", returning = "result")
+    public void doAfterReturning(JoinPoint joinPoint, JSONObject result) {
+
+        Signature signature = joinPoint.getSignature();
+        String classMethod = signature.getName();
+        log.info("方法{}执行完毕，返回参数为：{}", classMethod, result);
+        // 实际项目中可以根据业务做具体的返回值增强
+        result.put("value","业务增强");
+        log.info("对返回参数进行业务上的增强：{}", result);
     }
 }
